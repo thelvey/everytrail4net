8caae9278efa4f4c2d7a132f163984acddc387a9
thelvey <ashelvey@gmail.com>
2012-10-14 02:04:24 -0700

    Adding search test and again removing my API key

---
+++ Tests/Search/SearchTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using EveryTrailNET.Core;
+using EveryTrailNET.Core.QueryResponse;
+using EveryTrailNET.Objects;
+using System.Net;
+using System.IO;
+
+namespace Tests.Search
+{
+    [TestFixture]
+    public class SearchTests
+    {
+        [Test]
+        public void BasicSearch()
+        {
+            string searchQuery = "\"arizona trail\"";
+            SearchResponse response = Actions.Search(searchQuery);
+
+        }
+    }
+}
